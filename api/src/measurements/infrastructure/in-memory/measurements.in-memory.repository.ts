b598b87c9866ecabb6dfe5b18fbc2f199789bf3c
Samuel Gilles <samuel.gilles@mediflash.fr>
2025-07-09 19:15:04 +0200

    API: Create measurements in memory repository

---
+++ api/src/measurements/infrastructure/in-memory/measurements.in-memory.repository.ts
@@ -0,0 +1,26 @@
+import { randomUUID } from 'crypto';
+import type { MeasurementDto } from '../../domain/dtos/measurement.dto';
+import {
+  Measurement,
+  type MeasurementProps,
+} from '../../domain/models/measurement.model';
+import type { MeasurementsRepositoryPort } from '../../domain/services/measurements.repository.port';
+import type { EntityProps } from '../../../shared/entity';
+
+export class MeasurementsInMemoryRepository
+  implements MeasurementsRepositoryPort
+{
+  constructor(private measurements: EntityProps<MeasurementProps>[]) {}
+
+  get length() {
+    return this.measurements.length;
+  }
+
+  async create(
+    measurementProps: EntityProps<MeasurementProps>
+  ): Promise<EntityProps<MeasurementProps>> {
+    this.measurements = [...this.measurements, measurementProps];
+
+    return measurementProps;
+  }
+}
