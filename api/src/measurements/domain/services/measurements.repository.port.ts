f715bf202301fcac166b836b9d0712908a4c6c43
Samuel Gilles <samuel.gilles@mediflash.fr>
2025-07-09 19:14:40 +0200

    API: Create measurements domain service

---
+++ api/src/measurements/domain/services/measurements.repository.port.ts
@@ -0,0 +1,8 @@
+import type { EntityProps } from '../../../shared/entity';
+import type { MeasurementProps } from '../models/measurement.model';
+
+export interface MeasurementsRepositoryPort {
+  create(
+    data: EntityProps<MeasurementProps>
+  ): Promise<EntityProps<MeasurementProps>>;
+}
---
+++ api/src/measurements/domain/services/measurements.service.ts
@@ -0,0 +1,15 @@
+import { randomUUID } from 'crypto';
+import type { MeasurementDto } from '../dtos/measurement.dto';
+import type { MeasurementsRepositoryPort } from './measurements.repository.port';
+import { Measurement } from '../models/measurement.model';
+
+export class MeasurementsService {
+  constructor(private repository: MeasurementsRepositoryPort) {}
+
+  async create(data: MeasurementDto) {
+    const id = randomUUID();
+    const measurementProps = await this.repository.create({ id, ...data });
+
+    return new Measurement(measurementProps);
+  }
+}
